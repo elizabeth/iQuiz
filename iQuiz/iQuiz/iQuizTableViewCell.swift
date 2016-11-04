48c4a05c6443be415579b42b552744be998526a8
luludust <elizabethduong96@gmail.com>
2016-11-03 21:14:36 -0700

    added table cells

---
+++ iQuiz/iQuiz/iQuizTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  iQuizTableViewCell.swift
+//  iQuiz
+//
+//  Created by iGuest on 11/3/16.
+//  Copyright © 2016 Elizabeth. All rights reserved.
+//
+
+import UIKit
+
+class iQuizTableViewCell: UITableViewCell {
+    @IBOutlet weak var subjectLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
